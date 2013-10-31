cc542d088953520b35169e7048a5bdbd1109fb0d
Diego Izquierdo Fernandez <dgizquierdo@gmail.com>
2013-10-31 10:10:00 +0100

    changing ./routes/index

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-// Configuration
+//Routes
+require('./routes/index')(app);
 
+// Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -26,10 +28,6 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler());
 });
-
-// Routes
-
-app.get('/', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
